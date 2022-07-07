73195cd3192489f8b849bc06407104b10858749c
MaleeshaSulakshana <mpmsulakshana98@gmail.com>
2022-07-07 23:46:32 +0530

    Added new profile activity and implement ui

--- MOBILE/ESS/app/src/main/java/com/example/ess/SignInActivity.java
+++ MOBILE/ESS/app/src/main/java/com/example/ess/SignInActivity.java
@@ -2,13 +2,31 @@ package com.example.ess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private TextView textSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        textSignUp = (TextView) this.findViewById(R.id.textSignUp);
+
+        textSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
